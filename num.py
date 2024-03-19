c6262434e04305a76b01e2ea98e5bace2a926f30
ashab <ashabttembul@gmail.com>
2024-03-19 21:39:00 +0600

    AZZZA

--- num.py
+++ num.py
@@ -1,1 +1,6 @@
-ASKHAB TOP
+num1 = int(input("Введите первое число:"))
+num2 = int(input("Введите второе число:"))
+azza = num1 + num2
+assa = num1 - num2
+adda = num1 * num2
+print(azza, assa, adda)
